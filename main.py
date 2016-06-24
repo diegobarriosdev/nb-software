f3bb8fd6fe4412a9525c78e8967cda3039f9129c
diegoferbarrios <+>
2016-06-24 14:36:47 -0500

    Modificaciones Finales Para sustentación

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #importando las funciones
 from flask import Flask, render_template
 from colorlabeler import ColorLabeler
+from shapedetector import ShapeDetector
 from random import randint
 import time
 import winsound
@@ -10,8 +11,7 @@ import cv2
 import numpy
 import serial
 from bokeh.core.compat.mplexporter.renderers.base import Renderer
-from pycparser.c_ast import Switch
-from nose import case
+
 
 #Variables
 app = Flask(__name__)
@@ -43,21 +43,21 @@ def captureimage():
 def saveimage(name):
     for i in xrange(frames):
         temp=captureimage()
-    print("Taking image...")
+    #print("Taking image...")
     camcapture=captureimage()
     file="img/"+name+".png"
     cv2.imwrite(file,camcapture)
-    print("Imagen is saved")
+    #print("Imagen is saved")
        
 def cutimage(player,original,edited):
     imageoriginal="img/"+original+".png"
     imageedited="img/"+edited+".png"
     img=cv2.imread(imageoriginal)
-    if player=="com":
-        tmp=img[105:300, 25:220]        
+    if player=="player":
+        tmp=img[50:235, 80:270]        
         cv2.imwrite(imageedited,tmp)        
-    elif player=="player":
-        tmp=img[105:290, 400:570]
+    elif player=="com":
+        tmp=img[30:250, 410:640]
         cv2.imwrite(imageedited,tmp)
     cv2.imshow("Cut Image", tmp)
     cv2.waitKey(0)
@@ -67,12 +67,20 @@ def countcotours(img):
     dir="img/"+img+".png"
     image = cv2.imread(dir)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Image", gray)    
+    cv2.waitKey(0) 
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    cv2.imshow("Image", blurred)    
+    cv2.waitKey(0) 
     thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
- 
+    cv2.imshow("Image", thresh)    
+    cv2.waitKey(0) 
+    
+            
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]      
+    sd = ShapeDetector()
     
     for c in cnts:
     # compute the center of the contour
@@ -81,12 +89,15 @@ def countcotours(img):
         else:
             M = cv2.moments(c)
             cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"]) 
-            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-            cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-            cv2.putText(image, "center", (cX - 20, cY - 20),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-            nc=nc+1 
+            cY = int(M["m01"] / M["m00"])
+            shape = sd.detect(c) 
+            #if(shape=="square" or shape=="rectangle"):
+            if 1==1:
+                cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+                cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+                cv2.putText(image, "center", (cX - 20, cY - 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                nc=nc+1 
             cv2.imshow("Image", image)
             cv2.waitKey(0) 
     return nc
@@ -104,24 +115,30 @@ def findcolors(nc,im):
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
     
-    cl = ColorLabeler()
+    cl = ColorLabeler()    
+    sd = ShapeDetector()
     
     for c in cnts:
-        if cv2.contourArea(c)> 150:
+        if cv2.contourArea(c)< 800:
+            continue
+        else:
             M = cv2.moments(c)
             cX = int((M["m10"] / M["m00"]))
             cY = int((M["m01"] / M["m00"]))
-     
-            #shape = sd.detect(c)
+            shape = sd.detect(c)
             color = cl.label(lab, c)
-            if color==nc:
-                text = "{}".format(color)
-                cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-                cv2.putText(image, text, (cX, cY),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                cont=cont+1
-                cv2.imshow("Image", image)
-                cv2.waitKey(0)
+            print(shape)
+            print(color)
+           
+            if (shape=="square" or shape=="rectangle"):
+                if (color==nc):
+                    text = "{}".format(color)
+                    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+                    cv2.putText(image, text, (cX, cY),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                    cont=cont+1
+                    cv2.imshow("Image", image)
+                    cv2.waitKey(0)
     return cont
 
 def getnumber():
@@ -130,62 +147,59 @@ def getnumber():
 
 @app.route("/")
 def index():
-    return render_template('inicio.html')
-    
+    #captureimage()
+    #saveimage("prueba")
+    #cutimage("player","prueba","player1")
+    #cutimage("com","prueba","computer")
+    return render_template('inicio.html') 
+ 
 
 @app.route("/stg-a1-01")
-def iniciar():
-    #nbCOM=getnumber()
-    nbCOM=5
+def stgAct101():
+    nbCOM=getnumber()
     dat='A'+str(nbCOM)+'#'
     sendToArduino(dat)
     print("Numero Enviado: "+str(nbCOM))
     return render_template('stg-a1-01.html',nbCOM=nbCOM)
 
 @app.route("/rvoz-a1-01")
-def rvoz1():
+def stgAct102():
     captureimage()
     saveimage("originala101")
-    cutimage("com","originala101","a1m1com01")
-    #nbCOM=countcotours("a1m1com01")
-    nbCOM=5
+    cutimage("com","originala101","a1m1com")
+    nbCOM=countcotours("a1m1com")
     return render_template('rvoz-a1-01.html',nbCOM=nbCOM)
 
 #Verificar tablero Jugador
 @app.route("/rcam-a1-01")
-def rcam1():
+def stgAct103():
     captureimage()
     saveimage("originala101")
     cutimage("com","originala101","a1m1com")
     cutimage("player","originala101","a1m1player")
     nbPlayer=countcotours("a1m1player")
-    #nbCom=countcotours("a1m1com")
-    nbCom=5
-    nbPlayer=5
+    nbCom=countcotours("a1m1com")
     print("COM B:"+str(nbCom))
     print("PLA B:"+str(nbPlayer))
     return render_template("rcam-a1-01.html",nbCOM=nbCom,nbPlayer=nbPlayer)
 
 @app.route("/stg-a1-02")
-def act01m2():
+def stgAct104():
     return render_template('stg-a1-02.html')
 
 @app.route("/rcam-a1-02")
-def rcam2():
+def stgAct105():
     captureimage()
     saveimage("originala102")
     cutimage("player","originala102","a1m2player")
-    #nbPlayer=countcotours("a1m2player")
-    nbPlayer=1
-    print("NBloques A2M2: "+str(nbPlayer))
+    nbPlayer=countcotours("a1m2player")
     dat='A'+str(nbPlayer)+'#'
     print(dat)
     sendToArduino(dat)
-    print("Numero Bloques: " + str(nbPlayer))
     return render_template("rcam-a1-02.html",nbPlayer=nbPlayer)
     
 @app.route("/stg-a2-01")    
-def act02m101():
+def stgAct201():
     n=getnumber()
     sw=False
     while(sw!=True):
@@ -201,7 +215,7 @@ def act02m101():
     return render_template("stg-a2-01.html",tbCom=t)
 
 @app.route("/stg-a2-02")
-def act02m102():
+def stgAct202():
     captureimage()
     saveimage("originala201")  
     cutimage("com","originala201","coma2m1")
@@ -209,7 +223,7 @@ def act02m102():
     return render_template("stg-a2-02.html",rc=rc)
 
 @app.route("/stg-a2-03")
-def act02m103():
+def stgAct203():
     captureimage()
     saveimage("originala201")
     cutimage("com","originala201","coma2m1")
@@ -217,7 +231,7 @@ def act02m103():
     return render_template("stg-a2-03.html",gc=gc)
 
 @app.route("/stg-a2-04")
-def act02m104():
+def stgAct204():
     captureimage()
     saveimage("originala201")
     cutimage("com","originala201","coma2m1")
@@ -225,11 +239,11 @@ def act02m104():
     return render_template("stg-a2-04.html",bc=bc)
 
 @app.route("/stg-a2-05")
-def act02m105():
+def stgAct205():
     return render_template("stg-a2-05.html")
 
 @app.route("/stg-a2-06")
-def act02m106():
+def stgAct206():
     rc=0
     gc=0
     bc=0
@@ -249,11 +263,9 @@ def act02m106():
     return render_template("stg-a2-06.html",rc=rc,gc=gc,bc=bc,rp=rp,gp=gp,bp=bp)
 
 @app.route("/stg-a2-07")
-def act02m107():
+def stgAct207():
     return render_template("stg-a2-07.html")
 
  
 if __name__ == "__main__":
-    app.run(host=' 192.168.43.247')
-
-
+    app.run(host='172.15.34.216')
